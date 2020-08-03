1b5069e90c461f06870d9b95bd533a9b4dda96fd
Devan-Kerman <devan@cleverpath.com>
2020-08-03 11:30:43 -0500

    a

---
+++ testmod/src/main/java/net/devtech/test/nanoeventsaptestmod/NanoEventsApTestMod.java
@@ -0,0 +1,10 @@
+package net.devtech.test.nanoeventsaptestmod;
+
+import net.fabricmc.api.ModInitializer;
+
+public class NanoEventsApTestMod implements ModInitializer {
+	@Override
+	public void onInitialize() {
+
+	}
+}
